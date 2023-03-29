9191da44c651ab02b5daec57b17991b3d25db59a
Nagarjun G Narendra <nagarjun.gn@bshg.com>
2023-03-29 20:47:52 +0530

    Implemented 2 api calls.

---
+++ DadJokesIO/ConsumeAPI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using unirest_net.http;
+
+namespace DadJokesIO
+{
+    public class ConsumeAPI
+    {
+        public async Task GetRandomJokeAsync() 
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://dad-jokes.p.rapidapi.com/random/joke"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "aafe91bdc7msh37acef50ff2cf14p135b45jsna4071280281b" },
+        { "X-RapidAPI-Host", "dad-jokes.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+        }
+
+        public async Task GetRandomJokeCountAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://dad-jokes.p.rapidapi.com/joke/count"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "aafe91bdc7msh37acef50ff2cf14p135b45jsna4071280281b" },
+        { "X-RapidAPI-Host", "dad-jokes.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
+        }
+    }
+}
---
+++ DadJokesIO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DadJokesIO;
+
+Console.WriteLine("Hello, World!");
+
+ConsumeAPI api = new ConsumeAPI();
+await api.GetRandomJokeAsync();
+await api.GetRandomJokeCountAsync();
